1354ef3a2e5f5ee30e0d97aeb30227f1cc724b3f
Elton Rodrigues de Souza <120459876+eltonrds@users.noreply.github.com>
2023-10-11 20:31:42 -0300

    estrutura de cadastro de pf finalizado

--- src/PessoaJuridica.java
+++ src/PessoaJuridica.java
@@ -1,28 +1,21 @@
-public class PessoaJuridica {
+import java.time.LocalDate;
 
-    public String cpf;
+public class PessoaJuridica extends Pessoa {
+    public String razaoSocial;
+    public String cnpj;
+
     public LocalDate dataNasc;
 
-    @Override
-    public float CalcularImposto(float rendimento) {
-//        até 3000 - 3%
-//        de 3000 a 6000 - 5%
-//        de 6000 a 10000 - 7%
-//        acima de 10000 - 9%
+    public float CalcularImposto(float rendimento){
 
         if (rendimento <= 3000){
-            return .03f;
-
+            return rendimento * .03f;
         } else if (rendimento <= 6000) {
             return rendimento * .05f;
-
         } else if (rendimento <= 10000) {
             return rendimento * .07f;
-
         }else {
             return rendimento * .09f;
         }
-
     }
-
 }
